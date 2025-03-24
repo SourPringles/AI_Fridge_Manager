ea1ed75e464d4c6a81b97d4c73f1fab100276e46
KJM <chance0425@naver.com>
2025-03-24 10:20:58 +0900

    create_branch

--- demo_application/README.md
+++
@@ -1,3 +0,0 @@
-# demo_application
-
-A new Flutter project.
--- demo_application/android/app/src/main/kotlin/com/example/demo_application/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.demo_application
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
